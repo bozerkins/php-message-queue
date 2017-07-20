fc26c70c96a04c8f5b153921af1ab6f772ff3b37
Roman Shvets <pomaxa@gmail.com>
2017-07-20 14:37:23 +0300

    simplify checks, add custom Exceptions

---
+++ src/Exception/FileAccessError.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Exception;
+
+
+class FileAccessError extends \RuntimeException
+{
+}
---
+++ src/Exception/FileCreateError.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Exception;
+
+
+class FileCreateError extends \RuntimeException
+{
+}
